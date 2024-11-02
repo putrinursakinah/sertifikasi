73b12a17937dd0435ee9e4afeb519b8ef22bbcd0
RafaFiansyah <rafaputraanggifiansyah@gmail.com>
2024-11-02 22:42:51 +0700

    Chapta Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Session;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session; // Pastikan Session diimpor di sini
+
 class LoginController extends Controller
 {
     /*
@@ -39,9 +41,44 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        // Validasi input termasuk captcha
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'captcha' => 'required|captcha',
+        ]);
+
+        // Jika validasi berhasil, lanjutkan dengan logika login default
+        if (Auth::attempt($request->only('email', 'password'))) {
+            // Cek apakah akun tidak aktif setelah login berhasil
+            if (Auth::user()->status == 'Tidak Aktif') {
+                Auth::logout();
+                Session::flash('error', "Akun yang kamu gunakan sudah Tidak Aktif !");
+                return redirect('login');
+            }
+            return redirect()->intended($this->redirectPath());
+        }
+
+        // Jika login gagal, kembali ke halaman login dengan pesan error
+        return redirect()->back()->withInput($request->only('email', 'remember'))->withErrors([
+            'email' => 'Email atau password salah.',
+        ]);
+    }
+
+    /**
+     * Fungsi untuk memeriksa status pengguna setelah login.
+     */
     protected function authenticated()
     {
-        if(Auth::User()->status == 'Tidak Aktif') {
+        if (Auth::user()->status == 'Tidak Aktif') {
             Auth::logout();
             Session::flash('error', "Akun yang kamu gunakan sudah Tidak Aktif !");
             return redirect('login');
